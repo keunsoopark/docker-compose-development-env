951a607dbf07e1b4dc2c255699339598251af8c9
Keunsoo Park <keunsoo.park@arundo.com>
2020-02-03 09:35:18 +0100

    update in bash scripts

--- python-tests.sh
+++ python-tests.sh
@@ -1,2 +1,3 @@
+# When you want to run Python unittests only without bringing up all your services
 #!/bin/bash
 docker-compose -f docker-compose.common-python.yml -f docker-compose.dev-python.yml run --rm --entrypoint pytest python $*
--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,7 @@
+# Build all containers for development
 # dev-build:
   docker-compose -f docker-compose.common-python.yml -f docker-compose.dev-python.yml build 
+
+# Start all services
 # --no-cachedev:
   docker-compose -f docker-compose.common-python.yml -f docker-compose.dev-python.yml up
